d2a714d65417e6192839b40292dfb1f29288c9a9
mcallaghan <max.w.callaghan@gmail.com>
2015-10-17 12:39:14 +0200

    code typo

--- getting_tweets/search_tweets.R
+++ getting_tweets/search_tweets.R
@@ -18,11 +18,11 @@ coup_text <- sapply(coup, function(x) x$getText())
 coup_text_corpus <- Corpus(VectorSource(coup_text))
 
 #clean up
-r_stats_text_corpus <- tm_map(r_stats_text_corpus,
-                              content_transformer(function(x) iconv(x, to='UTF-8-MAC', sub='byte')),
+coup_text_corpus <- tm_map(coup_text_corpus,
+                              content_transformer(function(x) iconv(x, to='UTF-8', sub='byte')),
                               mc.cores=1
 )
-r_stats_text_corpus <- tm_map(r_stats_text_corpus, content_transformer(tolower), mc.cores=1)
-r_stats_text_corpus <- tm_map(r_stats_text_corpus, removePunctuation, mc.cores=1)
-r_stats_text_corpus <- tm_map(r_stats_text_corpus, function(x)removeWords(x,stopwords()), mc.cores=1)
-wordcloud(r_stats_text_corpus)
+coup_text_corpus <- tm_map(coup_text_corpus, content_transformer(tolower), mc.cores=1)
+coup_text_corpus <- tm_map(coup_text_corpus, removePunctuation, mc.cores=1)
+coup_text_corpus <- tm_map(coup_text_corpus, function(x)removeWords(x,stopwords()), mc.cores=1)
+wordcloud(coup_text_corpus)
