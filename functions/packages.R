aba3c50abc141b04301e12750c3b91e622d42692
mcallaghan <max.w.callaghan@gmail.com>
2015-12-08 22:41:37 +0100

    writing up the results in final paper

---
+++ functions/packages.R
@@ -0,0 +1,22 @@
+pkgs <- c('twitteR', 
+          'tm.lexicon.GeneralInquirer', 
+          'tm.plugin.sentiment', 
+          'dplyr', 
+          'ggplot2', 
+          'stringr', 
+          'wordcloud', 
+          'tm', 
+          'rworldmap', 
+          'ggmap', 
+          'textcat', 
+          'translateR', 
+          'tidyr', 
+          'lfe', 
+          'maptools', 
+          'rgeos', 
+          'nlme',
+          'knitr')
+
+getwd()
+
+#repmis::LoadandCite(pkgs, file = 'bib/Rpackagecitations.bib')
