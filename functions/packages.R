b5d24ec4f21f2476fc3dae3469e74ce3332fe0ef
mcallaghan <max.w.callaghan@gmail.com>
2015-12-11 13:22:05 +0100

    super nice website

--- functions/packages.R
+++ functions/packages.R
@@ -20,6 +20,6 @@ pkgs <- c('twitteR',
 
 wd <- getwd()
 
-bibfile <- paste0(strsplit(wd,"col_res_proj")[[1]][1],"col_res_proj/bib/Rpackagecitations.bib")
+bibfile <- paste0(strsplit(wd,"col_res_proj")[[1]][1],"col_res_proj/bib/RpackageCitations.bib")
 
 repmis::LoadandCite(pkgs, file = bibfile)
